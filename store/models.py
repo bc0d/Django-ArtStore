82414d84d3696f3e624b8316a9e5a8544e54132e
bc0d <athulkrishna446@gmail.com>
2024-09-11 20:54:18 +0530

    new files added

--- store/models.py
+++ store/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model) :
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
--- store/views.py
+++ store/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, "index.html")
+def home(request) :
+    return render(request, "home.html")
+
+def about(request) :
+    return render(request, 'about.html')
+
+def contact(request) :
+    return render(request, 'contact.html')
+
+def products(request) :
+    return render(request, 'products.html')
+
+def cart(request) :
+    return render(request, 'cart.html')
+
+def services(request) :
+    return render(request, 'services.html')
+
